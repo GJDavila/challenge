f37b2af2dffb65b98cfc29e224193305f46dafdd
grimaldo <gjdavilaguillen@gmail.com>
2019-04-27 13:45:12 -0500

    query to get users

--- ravn/src/query/SearchInfoUser.js
+++ ravn/src/query/SearchInfoUser.js
@@ -16,12 +16,12 @@ export const GET_CURRENT_USER = gql` query user($name: String!){
   }
   `;
   
-  /*    query to get all repositories of a user */
+  /*    query to get data from user or repository  */
   export const GET_REPOSITORIES_USER = gql` 
-  query user($name: String!){
+  query user($name: String!,$next: String!){
     user(login: $name) {
       
-       repositories(  first:5  ) {
+       repositories(  first:5 ,after:$next ) {
      
          totalCount
          edges {
@@ -43,3 +43,48 @@ export const GET_CURRENT_USER = gql` query user($name: String!){
   }
   `;
   
+
+/*
+  query to get data from user or repository
+  input:
+    nameUser : user name to search
+    typeData : USER/REPOSITORY   
+    totalPerPage: total number of elements per page
+    cursor: current position 
+
+
+*/
+export const GET_LIST_DATA =gql`
+query getListData($nameUser: String!, $typeData:SearchType!,$totalPerPage:Int!,$cursor:String!) {
+  search(query: $nameUser, type: $typeData, first:$totalPerPage ,after:$cursor) {
+    userCount
+    edges {
+      node {
+        
+        ... on User {
+          
+          avatarUrl
+          url
+          bio
+          name
+          email
+          location
+          login
+          id
+          
+         
+        }
+         ... on Repository {
+           
+          createdAt
+          url
+          name
+          description
+          id
+         
+        }
+      }cursor
+    }
+  }
+}
+`;
